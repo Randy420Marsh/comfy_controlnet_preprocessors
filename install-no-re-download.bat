545b2cc868c2641b31cd59eb4d8888dc9f39b413
Randy Marsh <46230578+Randy420Marsh@users.noreply.github.com>
2023-07-30 00:23:33 +0300

    Create install-no-re-download.bat

---
+++ install-no-re-download.bat
@@ -0,0 +1,19 @@
+@echo off
+CALL :NORMALIZEPATH "python3.exe"
+if exist "python3" (
+    echo Custom Python build of ComfyUI standalone executable detected:
+    echo "%RETVAL%"
+    echo --------------------------------------------------
+    python install-no-re-download.py %1
+) else (
+    for /f "tokens=*" %%i in ('where python') do set "PYTHON_PATH=%%i" & goto :done
+    :done
+    echo Custom Python not found. Use system's Python executable instead:
+    echo %PYTHON_PATH%
+    echo --------------------------------------------------
+    python3 install-no-re-download.py %1
+)
+
+:NORMALIZEPATH
+    SET RETVAL=%~f1
+    EXIT /B

c862192d8dec09ae36d7642bb8b85a61048a0505
Randy Marsh <46230578+Randy420Marsh@users.noreply.github.com>
2023-07-30 00:21:54 +0300

    Update and rename install-no-redownload.sh to install-no-re-download.sh

--- install-no-re-download.sh
+++ install-no-re-download.sh
@@ -3,10 +3,10 @@ if [ -e "python3" ]; then
     echo Custom Python build of ComfyUI standalone executable detected:
     echo "$(readlink -f "python3")"
     echo --------------------------------------------------
-    python3 install.py ${1:+"$1"} #One-liner is za bezt
+    python3 install-no-re-download.py ${1:+"$1"} #One-liner is za bezt
 else
     echo "Custom Python not found. Use system's Python executable instead:"
     echo "$(which python3)"
     echo --------------------------------------------------
-    python3 install.py ${1:+"$1"}
+    python3 install-no-re-download.py ${1:+"$1"}
 fi

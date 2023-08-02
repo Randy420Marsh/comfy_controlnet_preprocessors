5beb7a3a814334f73651620c573e944cfee745b2
Randy Marsh <46230578+Randy420Marsh@users.noreply.github.com>
2023-08-03 02:15:13 +0300

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,17 +1,17 @@
 @echo off
-CALL :NORMALIZEPATH "python3.exe"
-if exist "python3" (
+CALL :NORMALIZEPATH "..\..\..\python_embeded\python.exe"
+if exist "..\..\..\python_embeded\python.exe" (
     echo Custom Python build of ComfyUI standalone executable detected:
     echo "%RETVAL%"
     echo --------------------------------------------------
-    python install.py %1
+    ..\..\..\python_embeded\python.exe install.py %1
 ) else (
     for /f "tokens=*" %%i in ('where python') do set "PYTHON_PATH=%%i" & goto :done
     :done
     echo Custom Python not found. Use system's Python executable instead:
     echo %PYTHON_PATH%
     echo --------------------------------------------------
-    python3 install.py %1
+    python.exe install.py %1
 )
 
 :NORMALIZEPATH
